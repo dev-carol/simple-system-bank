0da23d64d0b2186506dfdf48ef3b7af74d7656dd
carol santos <contato.caroline1999@gmail.com>
2022-06-15 17:43:40 -0300

    feat: operation

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ const inquirer = require("inquirer")
 const chalk = require('chalk')
 const fs = require("fs")
 
-console.log("tste");
+operation()
+
+
+function operation(){
+    inquirer.prompt([
+        {
+            type:'list',
+            name:'action',
+            message:'O que você deseja fazer?',
+            choices: [
+                'Criar Conta',
+                'Consultar Saldo',
+                'Depositar',
+                'Saque',
+                'Sair'
+            ]
+        }
+    ]).then((answer)=>{
+        const action =answer['action']
+        console.log(action);
+    }).catch((err) => console.log(err))
+}
