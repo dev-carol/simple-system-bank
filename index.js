e528ba096c67a4af93e49f9d8481a9a2c22c79ff
carol santos <contato.caroline1999@gmail.com>
2022-06-15 19:14:48 -0300

    feat: deposite

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ function operation() {
       } else if (action === "Consultar Saldo") {
         getAccountBalance();
       } else if (action === "Saque") {
+        withdraw();
       } else if (action === "Sair") {
         console.log(chalk.bgBlue.black("Obrigado por usar o nodeBank"));
         process.exit();
@@ -173,11 +174,63 @@ function getAccountBalance() {
       const accountData = getAccount(accountName);
 
       console.log(
-        chalk.bgBlue.black(
-            `O saldo da sua conta é R$${accountData.balance}`
-            )
+        chalk.bgBlue.black(`O saldo da sua conta é R$${accountData.balance}`)
       ),
-      operation()
-    }) .catch((err) => console.log(err));
+        operation();
+    })
+    .catch((err) => console.log(err));
+}
 
+function withdraw() {
+  inquirer.prompt([
+    {
+      name: "accountName",
+      message: "Qual o nome da sua conta?",
+    },
+  ]).then((answer) =>{
+    const accountName = answer['accountName']
+   if(!checkAccount(accountName)){
+    return withdraw()
+   }
+
+   inquirer.prompt([
+    {
+        name:'amount',
+        message: 'Quanto você deseja sacar?'
+    }
+   ]).then((answer)=>{
+      const amount = answer['amount']
+       removeAmount(accountName, amount)
+     
+   })
+   .catch(err => console.log(err))
+
+  }).catch(err => console.log(err))
+}
+
+
+function removeAmount(accountName,amount){
+    const accountData = getAccount(accountName)
+
+    if(!amount){
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'));
+        return withdraw()
+    }
+
+    if(accountData.balance < amount){
+        console.log(chalk.bgRed.black('Valor indisponível!'));
+        return withdraw()
+
+    }
+    accountData.balance = parseFloat(accountData.balance) - parseFloat(amount)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function (err) {
+          console.log(err);
+        }
+    )
+    console.log(chalk.green(`Foi realizado um saque de R$${amount} da conta ${accountName}`));
+    operation()
 }
