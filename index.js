5039ce75504ef48c030d93a2c22f77a133993146
carol santos <contato.caroline1999@gmail.com>
2022-06-15 18:08:24 -0300

    feat: created account

--- index.js
+++ index.js
@@ -21,6 +21,47 @@ function operation(){
         }
     ]).then((answer)=>{
         const action =answer['action']
-        console.log(action);
+      if(action === 'Criar Conta'){
+        createAccount()
+        buildAccount()
+      }
+    }).catch((err) => console.log(err))
+}
+
+
+function createAccount(){
+ console.log(chalk.bgGreen.black('Estamos felizes que tenha nos escolhido como o seu banco!'));
+ console.log(chalk.green('Defina as opções da sua conta a seguir: '));
+  buildAccount()
+ 
+}
+
+function buildAccount(){
+    inquirer.prompt([
+        {
+            name:'accountName',
+            message: 'Digite um nome para a sua conta:'
+        }
+    ]).then(answer => {
+        const accountName = answer['accountName']
+       console.info(accountName)
+        if(!fs.existsSync('accounts')){
+            fs.mkdirSync('accounts')
+        }
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('A conta informada já existe, escolha um outro nome único e original '));
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, 
+        '{"balance": 0}',
+        function(err){
+            console.log(err);
+        },
+        )
+
+        console.log(chalk.green('Ebaa! Sua conta foi criada!'));
+        operation()
     }).catch((err) => console.log(err))
 }
